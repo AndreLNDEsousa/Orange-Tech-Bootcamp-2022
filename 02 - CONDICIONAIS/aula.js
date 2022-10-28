e683c4aa4cef21f6a90e6ba8aa782183be962692
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2022-10-28 12:17:48 -0300

    Update aula.js

--- 02 - CONDICIONAIS/aula.js
+++ 02 - CONDICIONAIS/aula.js
@@ -1,6 +1,15 @@
 
-const numero = 11;
+
+const numero = 0;
 
 const numeroPar = (numero % 2) === 0;
 
-console.log(numeroPar);
+if (numeroPar) {
+    console.log('Par')
+
+} else {
+    console.log('Impar')
+
+}
+
+console.log('Acabou!!!');
