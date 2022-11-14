a46ec9c35ae03860da0588bb4f927e5788e42475
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2022-11-14 17:47:08 -0300

    Update lista_exercicio_objeto.js

--- 05 - OBJETOS/lista_exercicio_objeto.js
+++ 05 - OBJETOS/lista_exercicio_objeto.js
@@ -49,8 +49,28 @@ class Pessoa {
         return this.peso / (this.altura * this.altura);
         
     } 
+
+    classificarImc() {
+       const imc = this.calcularImc();
+       if (imc < 18.2) {
+        return('Abaixo do peso');
+    }
+    else if (imc >= 18.5 && imc < 25) {
+        return('Peso normal');
+    }
+    else if (imc >= 25 && imc < 30) {
+        return('Acima do peso');
+        }
+    else if (imc >= 30 && imc < 40) {
+        return('Obeso');
+    }
+    else {
+        return('Obesidade Grave');
+    }
+        
+    }
         
 }
 
-new Pessoa('Andre', 73, 1.69);
-console.log(calcularImc().toFixed(1));
+const andre = new Pessoa('Andre', 70, 1.69);
+console.log(andre.classificarImc());
