743ce679a5a7bf3e50e4595dac820a179319469a
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2022-11-17 19:03:26 -0300

    exercício 1
    OK e melhorado com o conhecimento adquirido.

---
+++ 06 - ARRAY(Listas)/exercicio2_array.js
@@ -0,0 +1,10 @@
+/*2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.*/
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const element = numeros[i];
+    
+}
+
+/*3) Faça um programa que dado uma lista de nomes imprima os nomes que comecem com a letra "V".*/
